412fa0248e74b1036742082540026b64411cb75b
Buttless <sauravchala@gmail.com>
2024-02-22 13:11:51 -0500

    added textchangedevents

--- app/src/main/java/sunny/todolist/MainActivity.java
+++ app/src/main/java/sunny/todolist/MainActivity.java
@@ -6,11 +6,19 @@ import androidx.fragment.app.FragmentManager;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.Calendar;
 
@@ -23,9 +31,45 @@ public class MainActivity extends AppCompatActivity implements PickDateDialog.Sa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         currentTask = new Task();
+        initToggleButton();
+        setForEditing(false);
         initSettingsButton();
         initListButton();
         initPickDateButton();
+        initTextChangedEvents();
+
+    }
+
+    private void initToggleButton() {
+        final ToggleButton editToggle = (ToggleButton)findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setForEditing(editToggle.isChecked());
+            }
+        });
+    }
+
+    private void setForEditing(boolean enabled) {
+        EditText editSubject = findViewById(R.id.editTextSubject);
+        EditText editDescription  = findViewById(R.id.editTextDescription);
+        Button buttonChange = findViewById(R.id.buttonPickDate);
+        Button buttonSave = findViewById(R.id.buttonSave);
+        RadioButton radioLow = findViewById(R.id.radioButtonLow);
+        RadioButton radioMedium = findViewById(R.id.radioButtonMedium);
+        RadioButton radioHigh = findViewById(R.id.radioButtonHigh);
+
+        editSubject.setEnabled(enabled);
+        editDescription.setEnabled(enabled);
+        buttonChange.setEnabled(enabled);
+        buttonSave.setEnabled(enabled);
+        radioLow.setEnabled(enabled);
+        radioMedium.setEnabled(enabled);
+        radioHigh.setEnabled(enabled);
+
+        if(enabled) {
+            editSubject.requestFocus();
+        }
     }
 
     private void initSettingsButton() {
@@ -54,6 +98,13 @@ public class MainActivity extends AppCompatActivity implements PickDateDialog.Sa
 
     }
 
+    @Override
+    public void didFinishPickingDateDialog(Calendar selectedTime) {
+        TextView dueDate = findViewById(R.id.textViewDueDate);
+        dueDate.setText(DateFormat.format("MM/dd/yyyy", selectedTime));
+        currentTask.setDueDate(selectedTime);
+    }
+
     private void initPickDateButton() {
         Button pickDate = findViewById(R.id.buttonPickDate);
         pickDate.setOnClickListener(new View.OnClickListener() {
@@ -65,9 +116,61 @@ public class MainActivity extends AppCompatActivity implements PickDateDialog.Sa
             }
         });
     }
-    public void didFinishPickingDateDialog(Calendar selectedTime) {
-        TextView dueDate = findViewById(R.id.textViewDueDate);
-        dueDate.setText(DateFormat.format("MM/dd/yyyy", selectedTime));
-        currentTask.setDueDate(selectedTime);
+
+    private void initTextChangedEvents() {
+        final EditText etTaskSubject = findViewById(R.id.editTextSubject);
+        etTaskSubject.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //Auto-generated method stub
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentTask.setSubject(etTaskSubject.getText().toString());
+            }
+        });
+
+        final EditText etDescription = findViewById(R.id.editTextDescription);
+        etDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //Auto-generated method stub
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentTask.setDescription(etDescription.getText().toString());
+            }
+        });
+
+        final RadioGroup etGroup = findViewById(R.id.rgPriority);
+        etGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId)
+            {
+                RadioButton etPriority = (RadioButton)group.findViewById(checkedId);
+                boolean isChecked = etPriority.isChecked();
+                if (isChecked)
+                {
+                    currentTask.setPriority(etPriority.getText().toString());
+                }
+            }
+        });
+
+
     }
+
 }
